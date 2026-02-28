8fd5726cd64717f0669c689afa0fd3f8b2db8052
Chaitanya Arasada <chaitanyaarasada300@gmail.com>
2026-02-28 16:08:56 +0530

    Update AMI ID for app_server resource

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-0786adace1541ca80"
+  ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
 
   tags = {
